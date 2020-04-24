0d4e684e6cef60c02da1bf681e03ccd9eb66cf10
Armando <rivera.armando997@gmail.com>
2020-04-23 19:32:06 -0500

    Backend -Implementando Router para separar las peticiones por métodos

--- 13_Backend/server.js
+++ 13_Backend/server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const port = 3000;
 // import express from 'express'; // Sintaxis de ECMA 6
 
+// Route es una de las piezas más importantes
+// Nos permitirá separa las peticiones por cabeceras, métodos, URL, etc
+const router = express.Router();
 
 // Instanciamos la aplicación de express
 var app = express();
 
+app.use(router);
+
+// Con router podemos separar las peticion por ejemplo por metodos
+router.get('/', function(req, res) {
+	res.send('Hola desde GET');
+});
+
+router.post('/', function(req, res) {
+	res.send('Hola desde POST');
+});
+
+/* // Usando una ruta
 app.use('/', function(req, res) {
 	res.send('Hola');
 });
-
+*/
 
 app.listen(port);
 console.log('La aplicación está escuchando en http://localhost:'+port);
