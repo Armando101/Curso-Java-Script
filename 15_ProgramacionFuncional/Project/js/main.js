ec882c1896daa9480cfdf2fa97e0198e23feabc3
Armando <rivera.armando997@gmail.com>
2020-04-26 00:28:48 -0500

    Agegand funciones para agregar y limpiar registros en inputs

--- 15_ProgramacionFuncional/Project/js/main.js
+++ 15_ProgramacionFuncional/Project/js/main.js
@@ -2,31 +2,56 @@ const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
 
-const $description = document.getElementById('description');
-const $calories = document.getElementById('calories');
-const $carbs = document.getElementById('carbs');
-const $protein = document.getElementById('protein');
+const description = document.getElementById('description');
+const calories = document.getElementById('calories');
+const carbs = document.getElementById('carbs');
+const protein = document.getElementById('protein');
 
-$description.onkeypress = () => {
-	$description.classList.remove('is-invalid');
+let list = [];
+
+description.onkeypress = () => {
+	description.classList.remove('is-invalid');
 }
-$calories.onkeypress = () => {
-	$calories.classList.remove('is-invalid');
+calories.onkeypress = () => {
+	calories.classList.remove('is-invalid');
 }
-$carbs.onkeypress = () => {
-	$carbs.classList.remove('is-invalid');
+carbs.onkeypress = () => {
+	carbs.classList.remove('is-invalid');
 }
-$protein.onkeypress = () => {
-	$protein.classList.remove('is-invalid');
+protein.onkeypress = () => {
+	protein.classList.remove('is-invalid');
+}
+
+const add = () => {
+	const newItem = {
+		description: description.value,
+		calories: parseInt(calories.value),
+		carbs: parseInt(carbs.value),
+		protein: parseInt(protein.value)
+	}
+	list.push(newItem);
+	console.log(list);
+}
+	
+const cleanInputs = () => {
+	description.value = '';
+	calories.value = '';
+	carbs.value = '';
+	protein.value = '';
 }
 
 const validateInputs = () => {
-	$description.value ? '' : $description.classList.add('is-invalid');
-	$calories.value ? '' : $calories.classList.add('is-invalid');
-	$carbs.value ? '' : $carbs.classList.add('is-invalid');
-	$protein.value ? '' : $protein.classList.add('is-invalid');
 
-	if($description.value && $calories.value && $carbs.value && $protein.value) {
-		console.log('Everything is OK');
+	description.value ? '' : description.classList.add('is-invalid');
+	calories.value ? '' : calories.classList.add('is-invalid');
+	carbs.value ? '' : carbs.classList.add('is-invalid');
+	protein.value ? '' : protein.classList.add('is-invalid');
+
+	if(description.value && calories.value && carbs.value && protein.value) {
+		// console.log('Everything is OK');
+		add();
+		cleanInputs();
 	}
+
+
 }
