9107fbf40a80a68e79dee00bb13a812603416b99
Armando <rivera.armando997@gmail.com>
2020-04-26 00:50:45 -0500

    Function Composition para función que nos permite crear etiquetas para html

--- 15_ProgramacionFuncional/Project/js/main.js
+++ 15_ProgramacionFuncional/Project/js/main.js
@@ -1,6 +1,45 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+// Begining function composition
+/*
+	Conocemos como Function Composition a las funciones que obtenemos como resultado de combinar otras dos o más funciones.
+	El resultado de cada función es el argumento de la siguiente y así sucesivamente.
+*/
+
+// El objeto que recibe attrsToString es como el siguiente
+/*
+	{
+		tag: 'h1',
+		attr: {
+			class: 'title',
+			id: 'myId'
+		}
+	}
+
+	// Lo que retorna es:
+	tag = "h1" class="title" id="myId"
+*/
+
+const attrsToString = (obj = {}) => {
+	const keys = Object.keys(obj);
+	const attrs = [];
+
+	for(let i=0; i<keys.length; i++) {
+		let attr = keys[i];
+		attrs.push(`${attr}="${obj[attr]}"`);
+	}
+
+	const string = attrs.join('');
+	return string;
+}; 
+
+const tag = t => content => `<${t}>${content}</${t}>`;
+
+console.log(tag('h1')('title'));
+console.log(attrsToString({class: 'title'}));
+
+// End function composition
 
 const description = document.getElementById('description');
 const calories = document.getElementById('calories');
