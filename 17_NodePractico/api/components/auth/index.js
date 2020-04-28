d659dd5f487065e3bf07109b7790ff6c016794cd
Armando <rivera.armando997@gmail.com>
2020-04-28 11:48:24 -0500

    Conexión a la base de datos de mysql

--- 17_NodePractico/api/components/auth/index.js
+++ 17_NodePractico/api/components/auth/index.js
@@ -1,4 +1,5 @@
-const store = require('../../../store/dummy');
+//const store = require('../../../store/dummy');
+const store = require('../../../store/mysql');
 const controller = require('./controller');
 
 module.exports = controller(store);
--- 17_NodePractico/config.js
+++ 17_NodePractico/config.js
@@ -4,5 +4,11 @@ module.exports = {
 	},
 	jwt: {
 		secret: process.env.JWT_SECRET || 'notasecret'
-	}
+	},
+	mysql: {
+		host: process.env.MYSQL_HOST || 'localhost',
+		user: process.env.MYSQL_USER || 'root',
+		password: process.env.MYSQL_PASS || 'toor',
+		database: process.env.MYSQL_DB || 'proyecto_node',
+	},
 }
