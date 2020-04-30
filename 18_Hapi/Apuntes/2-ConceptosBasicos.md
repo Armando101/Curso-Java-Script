f46e131af978bc17c89b1d12aa6f0c215657352b
Armando <rivera.armando997@gmail.com>
2020-04-30 01:01:56 -0500

    Hapi -Sirviendo contenido estático con el plugin inert

--- 18_Hapi/Apuntes/2-ConceptosBasicos.md
+++ 18_Hapi/Apuntes/2-ConceptosBasicos.md
@@ -16,4 +16,25 @@ A través de este objeto se pueden especificar las cabeceras, tipo de documento 
 - .type(): Permite definir el tipo mime de la respuesta
 - .code(): Permite definir el código de estado de la respuesta
 
-Código: Ejemplo de respuesta simple y redirect.
+Código: Ejemplo de respuesta simple y redirect.  
+
+## Uso de plugins - Contenido estático
+
+Los plugins son módulos o archivos de Javascript creados generalmente por terceros, que le adicionan funcionalidades al framework base de **Hapi**.  
+
+Para implementar un plugin nuevo a nuestro proyecto, lo primero es importarlo en el index.js con la función **require()** de NodeJS. Luego es necesario registrarlo con ___awaitserver.register(plugin)___.
+
+Por el momento, incluiremos en nuestro proyecto los plugins de **Inert** y **Path**
+### Intalación
+```
+npm install inert
+```
+
+```
+const inert = require('inert')
+const path = require('path')
+```
+
+El plugin Inert extiende los métodos disponibles en el objeto *h*, y **Path** nos permite definir una ubicación relativa para todos los routes de nuestro proyecto, entre otras cosas.  
+### Documentación  
+[inert](https://www.npmjs.com/package/inert)
--- 18_Hapi/index.js
+++ 18_Hapi/index.js
@@ -1,33 +1,60 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi');
+const inert = require('inert');
+const path = require('path');
 
 async function init() {
     const server = Hapi.server({
         port: process.env.PORT || 3000,
-        host: process.env.HOST || 'localhost'
+        host: process.env.HOST || 'localhost',
+        routes: {
+        	files: {
+        		// Esto nos permite que todos lo archivos que sirvamos los haga desde public
+        		relativeTo: path.join(__dirname, 'public/')
+        	}
+        }
     });
 
-   server.route({
-      path: '/',
-      method: 'GET',
-      handler: (req, h) => {
-      	// Devuelvo un objeto de respuesta
-        return h.response('Hello World').code(200);
-      }
-   });
-
-   server.route({
-      path: '/redirect',
-      method: 'GET',
-      handler: (req, h) => {
-      	// Devuelvo un objeto de respuesta
-        return h.redirect('https://google.com');
-      }
-   });
 
 
-   	try {	
+   	try {
+   		// Tenemos que registrar el plugin cada que lo vamos a usar
+   		// No basta con sólo importarlo
+   		await server.register(inert);
+	   
+	   server.route({
+	      path: '/home',
+	      method: 'GET',
+	      handler: (req, h) => {
+	      	// Devuelvo un objeto de respuesta
+	        return h.file('index.html');
+	      }
+	   });
+
+	   server.route({
+	      path: '/redirect',
+	      method: 'GET',
+	      handler: (req, h) => {
+	      	// Devuelvo un objeto de respuesta
+	        return h.redirect('https://google.com');
+	      }
+	   });
+		
+		// Esta ruta es para que sirva los archivos css
+		server.route({
+			path: '/{params*}',
+			method: 'GET',
+			handler: {
+				directory: {
+					// Hace referencia al directorio actual
+					// public ya que lo habíamos definido arriba
+					path: '.',
+					index: ['index.html']
+				}
+			}
+		});
+
 	    await server.start();
    	} catch (error) {
    		console.log(error);
