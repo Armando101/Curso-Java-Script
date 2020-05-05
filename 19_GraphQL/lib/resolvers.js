d7d57b31fe5de38a81b914f514e950c542596006
Armando <rivera.armando997@gmail.com>
2020-05-05 00:47:24 -0500

    GraphQL -Custom Types

--- 19_GraphQL/lib/resolvers.js
+++ 19_GraphQL/lib/resolvers.js
@@ -3,9 +3,33 @@
 // Configurar los resolvers
 // Debe tener una propiedad igual al nombre, ésta será una función que se ejecuta cuando llamamos la quiery
 
+const courses = [
+	{
+		_id: 'anyid',
+		title: 'GraphQL',
+		teacher: 'Mi profesor',
+		description: 'Curso de GraphQL desde cero',
+		topic: 'Programación'
+	},
+	{
+		_id: 'anyid',
+		title: 'Backend con Node',
+		teacher: 'Mi profesor',
+		description: 'Curso de Backend con Node desde cero',
+		topic: 'Programación'
+	},
+	{
+		_id: 'anyid',
+		title: 'Backend con Hapi',
+		teacher: 'Mi profesor',
+		description: 'Curso de Backend con Hapi desde cero',
+		topic: 'Programación'
+	}
+];
+
 module.exports = {
-  hello: () => {
-    return 'Hola Mundo'
+  getCourses: () => {
+    return courses
   },
   saludo: () => {
     return 'Hola a todos'
