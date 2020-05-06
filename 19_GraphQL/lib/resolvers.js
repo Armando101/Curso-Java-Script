454b8d92967f4d1208e823ede7a1eb36d930c111
Armando <rivera.armando997@gmail.com>
2020-05-06 13:47:49 -0500

    GraphQL -Crear resolvers para los tipos anidados

--- 19_GraphQL/lib/resolvers.js
+++ 19_GraphQL/lib/resolvers.js
@@ -6,8 +6,10 @@
 
 const queries = require('./queries');
 const mutations = require('./mutations');
+const types = require('./types');
 
 module.exports = {
 	Query: queries,
-	Mutation: mutations
+	Mutation: mutations,
+	...types
 }
---
+++ 19_GraphQL/lib/types.js
@@ -0,0 +1,34 @@
+// Auí tendremos los resolvers para los tipos
+
+'use strict'
+
+const connectDb = require('./db');
+const { ObjectID } = require('mongodb');
+
+const types = {
+	Course: {
+		// Esta función se ejecuta para el tipo people que se encuentra en cursos
+		// Cuano se mande llamar el campo people de Course que contiene un arreglo de students
+		// Se ejecutará está función y se le pasará como argumento el arreglo de students
+		people: async ( { people } ) => {
+			let db, peopleData, ids;
+			try {
+				db = await connectDb();
+				ids = people ? people.map(id => ObjectID(id)) : [];
+				peopleData = ids.length > 0 ?
+				await db.collection('students').find(
+					{ _id: {$in: ids}} // Le pasamos el arreglo de ids
+				).toArray()
+				: [];
+				console.log(people);
+				console.log({people});
+			} catch(error) {
+				console.error(error);
+			}
+
+			return peopleData;
+		}
+	}
+}
+
+module.exports = types;
