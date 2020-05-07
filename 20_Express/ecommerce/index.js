2666231f33aa0e570f76f9bcd370f3441de89deb
Armando <rivera.armando997@gmail.com>
2020-05-07 16:27:19 -0500

    Express -Manejo de archivos estáticos

--- 20_Express/ecommerce/index.js
+++ 20_Express/ecommerce/index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const productsRouter = require('./routes/products');
 const port = 8000;
 
+// Definimos la ruta de los archivos estáticos
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
