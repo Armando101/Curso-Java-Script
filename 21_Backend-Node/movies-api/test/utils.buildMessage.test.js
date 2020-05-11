7c6423205a55cf0cf8b164a788f84cefdb5db37f
Armando <rivera.armando997@gmail.com>
2020-05-10 21:16:56 -0500

    Backend-Node -Test de utils usando TDD

---
+++ 21_Backend-Node/movies-api/test/utils.buildMessage.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const buildMessage = require('../utils/buildMessage');
+
+describe.only('utils - buildMessage', function() {
+	describe('When recives an entity and action', function() {
+		it('Should return the respective message', function() {
+			const result = buildMessage('movie', 'create');
+			const expect = 'movie created';
+			assert.strictEqual(result, expect);
+		});
+	});
+
+	describe('When recives an entity and an action and is a list ', function() {
+		it('Should return the respective message eith the entity in plural', function() {
+			const result = buildMessage('movie', 'list');
+			const expected = 'movies listed';
+			assert.strictEqual(result, expected);
+		})
+	})
+});
