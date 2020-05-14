4b1bf337cd99d3d7f3e565ce0b6d98c435361397
Armando <rivera.armando997@gmail.com>
2020-05-14 00:12:13 -0500

    Node Avanzado -Almacenar información del agente

--- 22_NodeAvanzado/hunab-mqtt/server.js
+++ 22_NodeAvanzado/hunab-mqtt/server.js
@@ -6,6 +6,8 @@ const redis = require('redis');
 const chalk = require('chalk');
 const db = require('hunab-db');	// Para instalarlo  npm i ../hunab-db/
 
+const { parsePayload } = require('./util');
+
 // Configuración del backend
 // Usaremos redis para el back
 const backend = {
@@ -27,6 +29,8 @@ const config = {
     dialect: 'postgres',
     logging: s => debug(s),
 }
+
+const clients = new Map();
 
 let Agent, Metric;
 
@@ -50,10 +54,55 @@ server.on('clientConnected', client => {
 
 server.on('clientDisconnected', client => {
 	debug(`Client Disconnected: ${client.id}`);
+	clients.set(client.id, null);	// Indico que un cliente se conecto
 });
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
 	debug(`Recived: ${packet.topic}`);
+
+	switch (packet.topic) {
+		case 'agent/Connected':
+		case 'agent/disconnected':
+			debug(`Payload: ${packet.payload}`)
+			break
+		case 'agent/message':
+			debug(`Payload: ${packet.payload}`);
+			const payload = parsePayload(packet.payload);
+
+			if (payload) {
+				payload.agent.connected = true;
+
+				let  agent;
+				// Si me llega un agente con información incorrecta se ignora
+				try {
+					agent = await Agent.createOrUpdate(payload.agent);
+				} catch (e) {
+					return handlerError(e);
+				}
+
+				debug(`Agent ${agent.uuid} saved`);
+
+				// Notiy Agent is Connected
+				if (!clients.get(client.id)) {
+					clients.set(client.id, agent);
+					server.published({
+						topic: 'agent/connected',
+						payload: JSON.stringify({
+							agent: {
+								uuid: agent.uuid,
+								name: agent.name,
+								hostname: agent.hostname,
+								pid: agent.pid,
+								connected: agent.connected
+							}
+						})
+					})
+				}
+
+			}
+			break;
+	}
+
 	debug(`Payload: ${packet.payload}`);
 });
 
@@ -73,6 +122,11 @@ function handlerFatalError(err) {
 	console.error(`${chalk.red('[fatal error]')} ${err.message}`);
 	console.error(err.stack);
 	process.exit(1);
+}
+
+function handlerError(err) {
+	console.error(`${chalk.red('[error]')} ${err.message}`);
+	console.error(err.stack);
 }
 
 process.on('uncaughtException', handlerFatalError);	// Cuando se lanzó una excepción que no fue manejada
