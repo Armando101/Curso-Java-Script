5f4a64034a658188b8011c1e6234fca17c359835
Armando <rivera.armando997@gmail.com>
2020-05-15 16:13:29 -0500

    Node Avanzado -Definiendo middleware de error

--- 22_NodeAvanzado/hunab-api/server.js
+++ 22_NodeAvanzado/hunab-api/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('hunab:api');
 const http = require('http');
 const chalk = require('chalk');
 const express = require('express');
@@ -12,6 +13,27 @@ const server = http.createServer(app);
 
 // Definimos las rutas
 app.use('/api', api);
+
+// Express error handler
+app.use((err, req, res, next) => {
+	debug(`Error: ${err.message}`);
+
+	// Not found errors
+	if (err.message.match(/not found/)) {
+		return res.status(404).send({ error: err.message })
+	}
+
+	res.status(500).send({ error: err.message });
+});
+
+function handleFatalError(err) {
+	console.log(`${chalk.red('[fatal error]')} ${err.message}`);
+	console.log(err.stack);
+	process.exit(1);
+}
+
+process.on('uncaughtException', handleFatalError);
+process.on('unhandledRejection', handleFatalError);
 
 server.listen(port, () => {
 	console.log(`${chalk.green('[hunab-api]')} server listening  http://localhost:${port}`);
