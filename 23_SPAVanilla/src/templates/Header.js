856708befa7df157d75a5800bca7fa94303eed98
Armando <rivera.armando997@gmail.com>
2020-05-21 13:19:21 -0500

    SPA Creando templates

---
+++ 23_SPAVanilla/src/templates/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+	const view = `
+		<div class="Header-main">
+			<div class="Header-logo">
+				<h1>
+					<a href="/">
+						100fifi.co
+					</a>
+				</h1>
+			</div>
+			<div class="Header-nav">
+				<a href="#/about/">
+					About
+				</a>
+			</div>
+		</div>
+	`;
+
+	return view;
+};
+
+export default Header;
