694df7fe82319e31ed4d50283ac34aaf30ffe1f4
Armando <rivera.armando997@gmail.com>
2020-05-21 21:49:58 -0500

    SPA Renderizando los personajes en el home

--- 23_SPAVanilla/src/pages/Home.js
+++ 23_SPAVanilla/src/pages/Home.js
@@ -1,14 +1,19 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+	const characters = await getData();
+	console.log(characters)
 	const view = `
 		<div class="Character">
-			<article class="Character-item">
-				<a href="#/1/">
-					<img src="image" alt="name" />
-					<h2>Name</h2>
-				</a>
-			</article>
-		</div>
-	`;
+			${characters.results.map(character=>`
+				<article class="Character-item">
+					<a href="#/${character.id}/">
+						<img src="${character.image}" alt="${character.name}" />
+						<h2>${character.name}</h2>
+					</a>
+				</article>
+			`).join('')}
+		</div>`;
 
 	return view;
 }
