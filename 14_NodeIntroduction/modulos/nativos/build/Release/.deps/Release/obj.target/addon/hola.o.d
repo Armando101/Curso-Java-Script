74e52cd6a8c59e18d4e278b56d55bc727750f157
Armando <rivera.armando997@gmail.com>
2020-07-23 15:36:51 -0500

    NodeIntroduction -Modulos, c++

---
+++ 14_NodeIntroduction/modulos/nativos/build/Release/.deps/Release/obj.target/addon/hola.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/addon/hola.o := g++ -o Release/obj.target/addon/hola.o ../hola.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/armando/.cache/node-gyp/14.5.0/include/node -I/home/armando/.cache/node-gyp/14.5.0/src -I/home/armando/.cache/node-gyp/14.5.0/deps/openssl/config -I/home/armando/.cache/node-gyp/14.5.0/deps/openssl/openssl/include -I/home/armando/.cache/node-gyp/14.5.0/deps/uv/include -I/home/armando/.cache/node-gyp/14.5.0/deps/zlib -I/home/armando/.cache/node-gyp/14.5.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hola.o.d.raw   -c
+Release/obj.target/addon/hola.o: ../hola.cc \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/node.h \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/v8.h \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/v8-internal.h \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/v8-version.h \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/v8config.h \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/v8-platform.h \
+ /home/armando/.cache/node-gyp/14.5.0/include/node/node_version.h
+../hola.cc:
+/home/armando/.cache/node-gyp/14.5.0/include/node/node.h:
+/home/armando/.cache/node-gyp/14.5.0/include/node/v8.h:
+/home/armando/.cache/node-gyp/14.5.0/include/node/v8-internal.h:
+/home/armando/.cache/node-gyp/14.5.0/include/node/v8-version.h:
+/home/armando/.cache/node-gyp/14.5.0/include/node/v8config.h:
+/home/armando/.cache/node-gyp/14.5.0/include/node/v8-platform.h:
+/home/armando/.cache/node-gyp/14.5.0/include/node/node_version.h:
