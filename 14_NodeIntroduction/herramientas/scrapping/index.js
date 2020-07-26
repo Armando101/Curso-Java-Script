e1f80043ca1b4e50b17642ad6a8982cae4ef6508
Armando <rivera.armando997@gmail.com>
2020-07-26 01:25:40 -0500

    Node Introduction -Herramientas pupeeter

---
+++ 14_NodeIntroduction/herramientas/scrapping/index.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+	console.log('Lanzamos navegador');
+
+	const browser = await puppeteer.launch({ headless: false });
+
+	const page = await browser.newPage();
+	await page.goto('https://es.wikipedia.org/wiki/Node.js');
+
+	const titulo1 = await page.evaluate(() => {
+		const h1 = document.querySelector('h1');
+		console.log(h1.innerHTML);
+		return h1.innerHTML;
+	});
+
+	console.log(titulo1);
+
+	console.log('Cerramos el navegador...');
+	browser.close();
+
+})();
