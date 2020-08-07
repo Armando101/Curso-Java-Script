53c80f47cb73530b2e4c3547a5b9cd9b3ebe327e
Armando <rivera.armando997@gmail.com>
2020-08-07 02:46:52 -0500

    Gulp -minify CSS

--- 26_Gulp/gulpfile.js
+++ 26_Gulp/gulpfile.js
@@ -1,16 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
 
 // Hacemos una tarea con gulp.task
 // On error nos permite mostrar los errores que pueda generar sass
 // Dest indica donde guardara lo compilado
 gulp.task('sass', async () => {
-	return gulp.src('./src/sass/*.sass')
+	return gulp.src('./src/sass/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('style_min', async () => {
+	return gulp.src('./src/sass/*.scss')
+		.pipe(sass())	// Convierte sass a css
+		.pipe(minifyCSS())	// minifica  el codigo
+		.pipe(concat('style_main_min.css'))	// concatena todos los archivos sass en uno solo
+		.pipe(gulp.dest('./dist/')); // Guarda el css
+});
+
 // Para que gulp compile cada que detecte un cambio usamos watch
 gulp.task('watch', async () => {
-	gulp.watch('./src/sass/*.sass', gulp.series('sass'));
+	gulp.watch('./src/sass/*.scss', gulp.series('style_min'));
 });
