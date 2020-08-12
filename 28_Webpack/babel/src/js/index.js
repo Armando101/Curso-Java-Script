61468615d350a721373ecdc436130eede7fac33a
Armando <rivera.armando997@gmail.com>
2020-08-12 00:21:51 -0500

    Webpack -Babel, JS Moderno

--- 28_Webpack/babel/src/js/index.js
+++ 28_Webpack/babel/src/js/index.js
@@ -1,11 +1,13 @@
 import '../css/index.css';
-import text from './text';
+import search from './search';
+import render from './render';
 
-text();
+const id = prompt('Quien es el pokemon');
 
-if (module.hot) {
-	module.hot.accept('./text.js', function() {
-		console.log('He recargado en caliente');
-		text();
-	});
-}
+search(id)
+	.then((data) => {
+		render(data);
+	})
+	.catch((err) => {
+		console.log('No hubo pokemon');
+	})
