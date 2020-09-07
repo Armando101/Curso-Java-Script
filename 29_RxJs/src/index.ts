5569eec6b9a3900c761065e454a71ed1b9ef0703
Armando <rivera.armando997@gmail.com>
2020-09-07 14:12:06 -0500

    RxJs encadenar operadores

--- 29_RxJs/src/index.ts
+++ 29_RxJs/src/index.ts
@@ -1,5 +1,5 @@
-import { range, from } from 'rxjs';
-import { filter } from 'rxjs/internal/operators/filter';
+import { range, from, fromEvent } from 'rxjs';
+import { filter, map } from 'rxjs/internal/operators';
 
 range(1, 10).pipe(
     filter((value, index) => {
@@ -29,3 +29,10 @@ const personajes: character[] = [
 ]
 
 from(personajes).pipe(filter((item: character) => item.tipo === 'heroe') ).subscribe(console.log);
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup').pipe(
+    map(event => event.code),   // KeyboardEvent, string
+    filter(key => key === 'Enter')
+)
+
+keyup$.subscribe(console.log);
