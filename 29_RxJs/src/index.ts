c639add177e5bbda398466cc6bf9840118c7bb69
Armando <rivera.armando997@gmail.com>
2020-09-09 23:01:37 -0500

    RxJs - Combinacion forkJoin Practica

--- 29_RxJs/src/index.ts
+++ 29_RxJs/src/index.ts
@@ -1,33 +1,14 @@
-import { of, interval, forkJoin } from "rxjs";
-import { take, delay } from "rxjs/operators";
+import { forkJoin, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { catchError } from "rxjs/operators";
 
-// Solo regresa un valor cuando todos los observables se completan
-// Regresa un arreglo con el ultimo elemento de cada observer
-const numbers$ = of(1, 2, 3, 4);
-const interval$ = interval(1000).pipe(take(3));
-const letters$ = of('a', 'b', 'c').pipe(delay(3500));
+const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_USER = 'Armando101';
 
-// forkJoin(
-//     numbers$,
-//     interval$,
-//     letters$
-// ).subscribe(response => {
-//     console.log('Numeros: ', response[0]);
-//     console.log('Intervalo: ', response[0]);
-//     console.log('Letras: ', response[0]);
-// });
-
-// Mismo ejercicio usando Objetos
-// forkJoin({
-//     numbers$,
-//     interval$,
-//     letters$
-// }).subscribe(console.log);
-
-
-// Otro caso con Objetos
 forkJoin({
-    num: numbers$,
-    interval: interval$,
-    letras: letters$
-}).subscribe(console.log);
+    usuario: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}`),
+    repos: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/repos`),
+    gist: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/gists`),
+})
+.pipe(catchError(error => of(error.message)))
+.subscribe(console.log);
