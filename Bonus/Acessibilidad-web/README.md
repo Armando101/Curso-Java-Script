de756cdd5c468463f298b04d76ca90b0a28ca877
Armano Rivera <rivera.armando997@gmail.com>
2020-11-23 12:21:03 -0600

    Iniciando proyecto

---
+++ Bonus/Acessibilidad-web/README.md
@@ -0,0 +1,13 @@
+# Curso de Accesibilidad Web
+
+En este curso, mejoraremos la accesibilidad un portafolio personal. Espero que aprendas mucho y que uses lo que aprendas aqui para implementar
+prácticas accesibles en tu propio portafolio!
+
+Usa esta URL para probar la accesibilidad con Lighthouse al principio del curso:
+https://gmzjuliana.github.io/curso-acessibilidad-web/index.html
+
+Usa esta URL para probar la accesibilidad al final:
+https://gmzjuliana.github.io/curso-acessibilidad-web/final.html
+
+Los apuntes de este curso se encuentran en la siguiente liga:
+https://www.notion.so/Accesibilidad-web-2e72e5c189e34e0e97cdc5fd1320833b
---
+++ Bonus/Acessibilidad-web/index.js
@@ -0,0 +1,63 @@
+window.onload = () => {
+  document.querySelector(".arrow-right").addEventListener("click", clickRight);
+  document.querySelector(".arrow-left").addEventListener("click", clickLeft);
+  document
+    .querySelector(".send-button")
+    .addEventListener("click", showNotification);
+  document.querySelectorAll(".project").forEach(element => {
+    element.addEventListener("click", e => openModal(e));
+  });
+  document.body.addEventListener("click", e => closeModal(e));
+};
+
+/** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
+function clickRight() {
+  const currentLeft = parseInt(
+    getComputedStyle(document.querySelector(".project-container")).left,
+    10
+  );
+  if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
+    return;
+  }
+  let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
+  document.querySelector(".project-container").style.left = `${newValue}px`;
+}
+
+/** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
+function clickLeft() {
+  const currentLeft = parseInt(
+    getComputedStyle(document.querySelector(".project-container")).left,
+    10
+  );
+  if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
+    return;
+  }
+  let newValue = currentLeft + 270;
+  document.querySelector(".project-container").style.left = `${newValue}px`;
+}
+
+/** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
+function showNotification() {
+  document.querySelector(".notification").style.display = "flex";
+  setTimeout(function() {
+    document.querySelector(".notification").style.display = "none";
+  }, 3000);
+}
+
+/** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
+function openModal(e) {
+  document.querySelector(".modal-container").style.display = "flex";
+}
+
+/** Esta funcion se llama para cerrar el modal */
+function closeModal(e) {
+  // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
+  if (
+    e.target.className.includes("project") ||
+    e.target.className === "modal"
+  ) {
+    return;
+  } else {
+    document.querySelector(".modal-container").style.display = "none";
+  }
+}
