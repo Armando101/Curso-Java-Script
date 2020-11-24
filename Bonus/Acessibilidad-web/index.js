5377b882e504cb904a7bd15f80eff90c73c7fb92
Armano Rivera <rivera.armando997@gmail.com>
2020-11-24 12:25:02 -0600

    Accesibilidad -JS Tabindex

--- Bonus/Acessibilidad-web/index.js
+++ Bonus/Acessibilidad-web/index.js
@@ -21,6 +21,21 @@ function clickRight() {
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project1').setAttribute('tabindex', '-1');
+      document.querySelector('.project4').removeAttribute('tabindex');
+      break;
+      case -540:
+        document.querySelector('.project2').setAttribute('tabindex', '-1');
+        document.querySelector('.project5').removeAttribute('tabindex');
+      break;
+  
+    default:
+      break;
+  }
+  
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
@@ -34,6 +49,21 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project5').setAttribute('tabindex', '-1');
+      document.querySelector('.project2').removeAttribute('tabindex');
+      break;
+      case 0:
+        document.querySelector('.project4').setAttribute('tabindex', '-1');
+        document.querySelector('.project1').removeAttribute('tabindex');
+      break;
+  
+    default:
+      break;
+  }
+
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
