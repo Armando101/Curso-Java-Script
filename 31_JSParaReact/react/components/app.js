022822e14846c753d41b4f316172770dfe3b862a
Armano Rivera <rivera.armando997@gmail.com>
2020-12-12 23:11:06 -0600

    React para JS -POO, Añadiendo styles components

--- 31_JSParaReact/react/components/app.js
+++ 31_JSParaReact/react/components/app.js
@@ -1,6 +1,7 @@
 import { Component } from "../lib/react.js";
 import User from "./user.js";
 import Wrapper from "./wrapper.js";
+import UserStyled from "./user-styled.js";
 
 class App extends Component {
   render() {
@@ -14,6 +15,10 @@ class App extends Component {
               avatar: '../images/ash.jpg'
             }).render()}
           `
+        }).render()}
+        ${new UserStyled({
+            name: 'Ash',
+            avatar: '../images/ash.jpg'
         }).render()}
       </div>
     `;
