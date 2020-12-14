1ce608fc106019063f5b4b67badef2d333a5e8c3
Armano Rivera <rivera.armando997@gmail.com>
2020-12-14 14:49:20 -0600

    React para JS -POO, Version mejorada de React, Soporte de eventos

--- 31_JSParaReact/5_React/components/user.js
+++ 31_JSParaReact/5_React/components/user.js
@@ -1,14 +1,37 @@
 import { Component, createElement } from "../lib/react/index.js";
 
 class User extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   // Pasamos el this de User a la función
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // Tenemos que cambiar el this porque es una función normal y el this ocupa el del HtmlElement
+  handleClick(event) {
+    console.log(this.props.name);
+    // console.log(this);
+  }
+
+  // Aquí this es el de User porque usamos una arrow function
+  // handleClick = (event) => {
+  //   console.log(this);
+  // }
   render() {
     const {avatar, name} = this.props;
-    return createElement('div', {class: 'user', children: [
-      createElement('div', {class: 'avatar', children:
-        createElement('img', {src: avatar})
-      }),
-      createElement('h2', null, name)
-    ]});
+    return createElement('div', {
+      class: 'user', children: [
+        createElement('div', {class: 'avatar', children:
+          createElement('img', {src: avatar})
+        }),
+        createElement('h2', null, name)
+      ],
+      onClick: this.handleClick.bind(this),
+      // Una opción sería agregar el evento de esta manera
+      // onClick: function () {
+      //   console.log('Hello World');
+      // }
+    });
   }
 }
 
--- 31_JSParaReact/5_React/lib/react/src/ReactElement.js
+++ 31_JSParaReact/5_React/lib/react/src/ReactElement.js
@@ -1,5 +1,20 @@
 import { render } from "../../react-dom.js";
 
+/**
+ * Coloca eventos en un elemento html
+ * @param {HtmlElement} element elemento html para colocar el evento
+ * @param {Event} event evento
+ * @param {Function} callback función que se llamará cuando ocurra el evento
+ */
+function setEvents(element, event, callback) {
+  element.addEventListener(event, callback);
+}
+
+/**
+ * Renderiza un elemento o un arreglo de elementos hijos en el navegador
+ * @param {HtmlElement} children elemento o arreglo de elementos hijos
+ * @param {HtmlElement} container Contenedor o elemnto padre
+ */
 function renderChildren(children, container) {
   if (Array.isArray(children)) {
     return children.forEach(child => render(child, container));
@@ -8,13 +23,22 @@ function renderChildren(children, container) {
 }
 
 /**
- * Coloca un atributo a un elemento HTML
+ * Esta función puede:
+ * - Colocar un atributo, en el elemento html
+ * - Agragar un evento al elemento
+ * - Andiar elementos html
  * @param {string} prop tipo de atributo que tendrá el elemento HTML
  * @param {string} value valor del atributo
  * @param {HTMLElement} element elemento html al que se le colocará el atributo
  * @returns {HTMLElement} elemento html con el atributo
  */
 function setProperties(prop, value, element) {
+  // Event support
+  if (prop.startsWith('on')) {
+    const event = prop.replace('on', '').toLocaleLowerCase();
+    return setEvents(element, event, value);
+  }
+
   // Support for childre
   if (prop === 'children') {
     return renderChildren(value, element);
