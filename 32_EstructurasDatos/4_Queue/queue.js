f9bc407b2470cb22570bdf2f5a1f372951d587fa
Armano Rivera <rivera.armando997@gmail.com>
2020-12-28 23:32:27 -0600

    Estructuras de datos -Queue

--- 32_EstructurasDatos/4_Queue/queue.js
+++ 32_EstructurasDatos/4_Queue/queue.js
@@ -4,3 +4,38 @@ class Node {
     this.next = null;
   }
 }
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return;
+    }
+    this.first = this.first.next;
+    this.length--;
+  }
+
+}
+
+const myQueue = new Queue();
