59bc58e702f99fdc0a46965ee675f88484cc1abd
Armano Rivera <rivera.armando997@gmail.com>
2021-01-20 12:22:38 -0600

    02-WorkShop-LazyLoading -Cargando las imágenes con JS

--- 33_ManejoDOM/02_Workshop-Lazy/src/index.js
+++ 33_ManejoDOM/02_Workshop-Lazy/src/index.js
@@ -1,6 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// Crear (1) imagen y agregarla al contendor
+const minimun = 1;
+const maximum = 122;
+const random = () => Math.floor((Math.random() * (maximum - minimun)) + 1);
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+  const container = document.createElement('div');
+  container.className = 'p-4';
+
+  const image = document.createElement('img');
+  image.className = 'mx-auto';
+  image.width = '320';
+  image.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  container.appendChild(image);
+
+  return image;
+}
+
+const newImage = createImageNode();
+const mountNode = document.getElementById('images');
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.append(newImage);
+}
+
+addButton.addEventListener('click', addImage);
