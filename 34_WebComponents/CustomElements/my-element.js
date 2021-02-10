032e165f111975e62246577d59a9d106c364fcfa
Armando Rivera <rivera.armando997@gmail.com>
2021-02-10 12:44:53 -0600

    34_WC; Atributos

--- 34_WebComponents/CustomElements/my-element.js
+++ 34_WebComponents/CustomElements/my-element.js
@@ -1,19 +1,22 @@
 class myElement extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' })
+    this.attachShadow({ mode: 'open' });
+
+    this.title = this.getAttribute('title');
+    this.parrafo = this.getAttribute('parrafo');
+    this.img = this.getAttribute('img');
   }
 
   getTemplate() {
     const template = document.createElement('template');
     template.innerHTML = `
       <section>
-        <h2>
-          <slot name="title"></slot>
-        </h2>
-        <p>
-          <slot name="parrafo"></slot>
-        </p>
+        <h2>${this.title}</h2>
+        <div>
+          <p>${this.parrafo}</p>
+          <img src=${this.img} alt=""/>
+        </div>
       </section>
       ${this.getStyles()}
     `;
