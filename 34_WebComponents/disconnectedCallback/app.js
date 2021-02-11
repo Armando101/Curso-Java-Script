b794749659a54f2bae09099009c8290b11974936
Armando Rivera <rivera.armando997@gmail.com>
2021-02-11 10:25:55 -0600

    34_WC; disconnectedCallback

---
+++ 34_WebComponents/disconnectedCallback/app.js
@@ -0,0 +1,18 @@
+class MyCustomElement extends HTMLElement {
+  constructor() {
+    super();
+    console.log('Hola desde el contructor - Memoria');
+  }
+
+  connectedCallback() {
+    console.log('Hola desde el DOM');
+  }
+
+  disconnectedCallback() {
+    console.log('Adios del DOM');
+  }
+}
+
+customElements.define('my-custome-element', MyCustomElement);
+
+document.querySelector('my-custome-element').remove();
