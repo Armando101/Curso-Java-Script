5a161a2a05c0d950304318d440bbd979bfbf79d9
Armando Rivera <rivera.armando997@gmail.com>
2021-02-11 11:38:59 -0600

    34_WC; Custom properties

---
+++ 34_WebComponents/05_CustomProperties/my-element.js
@@ -0,0 +1,74 @@
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: 'open',
+    });
+  }
+
+  getStyles() {
+    return `
+      <style>
+        :host {
+          --primary-color: tomato;
+          --secondary-color: salmon;
+          --heading-primary: 30px;
+          --heading-secondary: 25px;
+
+          display: inline-block;
+          width: 100%;
+          min-width; 300px;
+          max-width: 450px;
+        }
+
+        section {
+          background: var(--primary-color);
+        }
+
+        section div {
+          background: var(--secondary-color);
+        }
+
+        h1 {
+          font-size: var(--heading-primary);
+        }
+
+        p {
+          font-size: var(--heading-secondary);
+        }
+
+      </style>
+    `;
+  }
+
+  getTemplate() {
+    const template = document.createElement('template');
+
+    template.innerHTML = `
+      <section>
+        <h1>
+          <slot name='title'></slot>
+        </h1>
+
+        <div>
+          <p>
+            <slot name='parrafo'></slot>
+          </p>
+        </div>
+      </section>
+      ${this.getStyles()}
+    `;
+
+    return template;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+}
+
+customElements.define('my-element', MyElement);
