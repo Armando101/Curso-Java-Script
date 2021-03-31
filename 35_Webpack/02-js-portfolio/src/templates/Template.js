2f3d41878df99fcbc307653c64c9e6d806ce6ca9
Armando Rivera <rivera.armando997@gmail.com>
2021-03-31 17:44:39 -0600

    35_Webpack; copiar archivos

--- 35_Webpack/02-js-portfolio/src/templates/Template.js
+++ 35_Webpack/02-js-portfolio/src/templates/Template.js
@@ -1,4 +1,4 @@
-import getData from '../utils/getData.js';
+import getData from "../utils/getData.js";
 
 const Template = async () => {
   const data = await getData();
@@ -24,13 +24,13 @@ const Template = async () => {
         </div>
         <div class="card_social">
           <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+            <img src="assets/images/twitter.png" />
           </a>
           <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+            <img src="assets/images/github.png" />
           </a>
           <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+            <img src="assets/images/instagram.png" />
           </a>
         </div>
       </div>
--- 35_Webpack/02-js-portfolio/webpack.config.js
+++ 35_Webpack/02-js-portfolio/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -33,5 +34,13 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
   ],
 };
