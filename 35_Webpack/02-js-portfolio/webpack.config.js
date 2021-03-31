1e7f5ca45fd941743e7fcec46baf5b345a541bc1
Armando Rivera <rivera.armando997@gmail.com>
2021-03-31 17:19:19 -0600

    35_Webpack; HTML con webpack

--- 35_Webpack/02-js-portfolio/webpack.config.js
+++ 35_Webpack/02-js-portfolio/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -20,4 +21,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
