692f03da915b2e99df43baf460276e144cdc9a3e
Armando Rivera <rivera.armando997@gmail.com>
2021-03-31 18:04:13 -0600

    35_Webpack; loader fonts

--- 35_Webpack/02-js-portfolio/webpack.config.js
+++ 35_Webpack/02-js-portfolio/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
@@ -28,6 +29,20 @@ module.exports = {
       {
         test: /\.png/,
         type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimeType: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
       },
     ],
   },
