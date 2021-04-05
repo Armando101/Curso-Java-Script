db8821a1b9cb1ea8d4983f64c962b5e6fd7b14cb
Armando Rivera <rivera.armando997@gmail.com>
2021-04-05 17:52:26 -0500

    35_Webpack; watch

--- 35_Webpack/02-js-portfolio/webpack.config.dev.js
+++ 35_Webpack/02-js-portfolio/webpack.config.dev.js
@@ -12,6 +12,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
+  // watch: true,
   resolve: {
     extensions: [".js"],
     alias: {
