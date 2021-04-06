15718eb69fd788b98d82d0f78de7fc6a807c8c40
Armando Rivera <rivera.armando997@gmail.com>
2021-04-06 16:40:31 -0500

    35_Webpack; bundle analyzer

--- 35_Webpack/02-js-portfolio/webpack.config.dev.js
+++ 35_Webpack/02-js-portfolio/webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer");
 
 module.exports = {
   entry: "./src/index.js",
@@ -73,6 +74,7 @@ module.exports = {
       ],
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
