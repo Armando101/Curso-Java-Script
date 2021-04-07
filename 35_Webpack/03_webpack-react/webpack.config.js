08a9b5322794d51bffa16cfd7837a428d99e0236
Armando Rivera <rivera.armando997@gmail.com>
2021-04-07 15:24:36 -0500

    35_Webpack; Agregar CSS

--- 35_Webpack/03_webpack-react/webpack.config.js
+++ 35_Webpack/03_webpack-react/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -23,12 +24,19 @@ module.exports = {
         test: /\.html$/,
         use: [{ loader: "html-loader" }],
       },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
     }),
   ],
   devServer: {
